044bd4a57070866d0ec6a978e12e18de64dfd44b
duynk96 <92417458+duynk96@users.noreply.github.com>
2025-11-14 11:17:23 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,8 +9,8 @@ let package = Package(
         .library(name: "GTVSdkIosFull", targets: ["GTVSdkIosFullWrapper"])
     ],
     dependencies: [
-       .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "12.3.0"),
-       .package(url: "https://github.com/airbridge/airbridge-ios-sdk.git", from: "4.8.1"),
+       .package(url: "https://github.com/firebase/firebase-ios-sdk.git", exact: "12.3.0"),
+       .package(url: "https://github.com/airbridge/airbridge-ios-sdk.git", exact: "4.8.1"),
        // .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "12.11.0")
     ],
     targets: [
